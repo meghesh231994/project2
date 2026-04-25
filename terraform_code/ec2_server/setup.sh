7f130295172873044f6109930bb01ef4be0d3cf7
meghesh231994 <megheshbhusari966@gmail.com>
2026-04-25 17:50:13 +0530

    Fix: Jenkins repo echo command - move URL inside quotes in setup.sh

--- terraform_code/ec2_server/setup.sh
+++ terraform_code/ec2_server/setup.sh
@@ -46,7 +46,7 @@ java -version
 # REF: https://www.jenkins.io/doc/book/installing/linux/#debianubuntu
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update -y                                   # to update package
 sudo apt install jenkins -y                          # to install jenkins
